9db79cdfae5e7df6ce3e1b4da14bd326d35bb696
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-01-24 16:37:37 +0500

    say hello button

--- src/components/Myinfo.jsx
+++ src/components/Myinfo.jsx
@@ -3,29 +3,32 @@ import React from 'react'
 export default function Myinfo() {
   return (
     <>
-      <div className="w-full min-h-screen  bg-gradient-to-tl from-white-500 via-[rgb(192,149,239)] to-[rgb(192,149,239)] ">
-        <div className="w-full h-[100dvh]  flex flex-col  md:flex-row ">
+      <div className="w-full min-h-screen  ">
+        <div className="w-full h-[100dvh]  flex flex-col  md:flex-row bg-gradient-to-tl from-white-500 via-[rgb(192,149,239)] to-[rgb(192,149,239)] ">
           <div className="  md:h-[100dvh] md:w-1/2  w-full h-3/4 flex flex-col  items-center justify-center ">
-            <div className="md:h-[60dvh] h-1/2 w-3/4 flex flex-col md:mt-30 ">
+            <div className="md:h-[60dvh] h-1/2 w-3/4 flex flex-col md:mt-30">
               <h1 className="md:text-3xl text-xl  font-bold">
                 HEY! <br /> I AM MERNSTACK DEVELOPER
               </h1>
               <p className="md:text-lg text-sm">
                 a passionate web developer creating reponsive
-                <br /> and modren web experiences. I specialize in <br />
+                 and modren web experiences. I specialize in
                 React, Tailwind, CSS, and building user-friendly
-                <br /> interfaces.
+               interfaces.
               </p>
+              <botton className="lg:h-[9dvh]  h-[6dvh] lg:w-[9svw]  w-[23svw] rounded bg-[rgb(172,129,239)] border text-white text-base lg:mt-10 mt-4 flex items-center justify-center lg:text-xl text-sm">
+                SAY HELLO!
+              </botton>
             </div>
-            <div className="h-1/3 w-full">
-              <div className="  md:text-xl text-sm md:h-[10dvh] h-[10dvh] w-full text-white  grid grid-cols-3 gap-1 md:px-15  px-10">
-                <dv className=" flex justify-center items-center  bg-[rgb(172,129,239)]  border  border-solid border-1">
+            <div className="h-1/4 w-full">
+              <div className="  md:text-xl text-sm lg:h-[10dvh] h-[10dvh] w-full text-white  grid grid-cols-3 gap-1 md:px-15  px-8 lg:mt-1 mt-5">
+                <dv className=" flex justify-center items-center  bg-[rgb(172,129,239)]  border ">
                   experience
                 </dv>
-                <div className=" flex justify-center items-center border  border-solid border-1 bg-[rgb(172,129,239)]">
+                <div className=" flex justify-center items-center border   bg-[rgb(172,129,239)] ">
                   project
                 </div>
-                <div className=" flex justify-center items-center border border-rounded border-white border-solid border-1 bg-[rgb(172,129,239)] ">
+                <div className=" flex justify-center items-center border   bg-[rgb(172,129,239)]  ">
                   happy clients
                 </div>
               </div>
@@ -35,7 +38,7 @@ export default function Myinfo() {
             <div className=" ml-10 md:h-[60dvh] h-[35dvh] w-[35dvh]  md:w-[60dvh] flex items-center justify-center border  border-none rounded-full  bg-gradient-to-tr from-[rgb(61,53,68)] to[rgba(198,252,216,1)]">
               <div className="md:h-[55dvh] md:w-[55dvh]  h-[33dvh] w-[33dvh] bg-gradient-to-b from-[rgba(193,142,232,1)] via-[rgba(193,142,232,1)] to-[rgba(218,210,234,1)] border border-none rounded-full flex items-center justify-center">
                 <div className="md:h-[50dvh] md:w-[50dvh] h-[30dvh] w-[30dvh] flex items-center justify-center border rounded-full border-dashed  border-2 border-purple-400">
-                  <div className="bg-[rgba(164,118,212,1)] md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full">
+                  <div className="bg-[rgb(172,129,239)] md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full">
                     <img
                       className="md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full  "
                       src="/assets/images2.png"
@@ -46,6 +49,7 @@ export default function Myinfo() {
             </div>
           </div>
         </div>
+        <div className="w-full h-[40dvh] ">h</div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,11 @@ export default function Navbar() {
       <>
         <nav
           className="w-full overflow-x-hidden h-13 bg-gray-50 sticky top-0 z-10 transition duration-300 "
-          data-headlessui-state="open"
-          data-open
+       
         >
-          <div className=" flex items-center font-medium text-sm md:text-base px-5 md:px-2  gap-3 md:gap-5 ">
+          <div className=" flex items-center font-medium md:text-normal font-bold px-5 md:px-2  gap-8 md:gap-5 ">
             <img className="h-[4svh] md:ml-20 " src="/assets/k.png"></img>
-            <h1 className=" mr-0 md:mr-130 text-purple-900 flex font-bold hidden lg:block">
+            <h1 className=" mr-0 md:mr-100 text-purple-900 flex font-bold  text-xl hidden lg:block">
               khadija Zafar
             </h1>
 
@@ -52,7 +51,7 @@ export default function Navbar() {
               Blog
             </Link>
 
-            <button className="md:h-[5svh] h-[3svh] md:w-[7svw] rounded bg-purple-500 text-white text-base ">
+            <button className="md:h-[5svh] h-[3svh] md:w-[7svw] rounded bg-purple-400 text-white text-base ">
               <a href="">Contact</a>
             </button>
           </div>
