9934ced02c081fe47a795d669f152823490598ed
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-04-04 21:06:13 +0500

    fixing ui

--- src/components/Myinfo.jsx
+++ src/components/Myinfo.jsx
@@ -3,25 +3,29 @@ import React from 'react'
 export default function Myinfo() {
   return (
     <>
-      <div className="w-full min-h-screen  ">
-        <div className="w-full h-[100dvh]  flex flex-col  md:flex-row bg-gradient-to-t from-white-500  to-[rgb(192,149,239)] ">
-          <div className="  md:h-[100dvh] md:w-1/2  w-full h-3/4 flex flex-col  items-center justify-center ">
+      <div className="w-full min-h-screen overflow-x-hidden overscroll-x-none ">
+        <div className="w-full h-dvh  flex flex-col  md:flex-row bg-black text-white ">
+          <div className="  md:w-1/2  w-full h-1/2 md:h-3/4 flex flex-col  ml-5 md:ml-10 justify-center">
             <div className="md:h-[60dvh] h-1/2 w-3/4 flex flex-col md:mt-30">
-              <h1 className="md:text-3xl text-xl  font-bold">
-                HEY! <h1 className="text-purple-800"> I AM MERNSTACK</h1>
+              <h1 className="md:text-3xl text-md text-xl  font-bold">
+                HEY!
+                <span className="text-purple-800 md:text-3xl text-2xl">
+                  {" "}
+                  I AM MERNSTACK{" "}
+                </span>
                 DEVELOPER
               </h1>
-              <p className="md:text-lg text-sm">
+              <p className="md:text-lg text-sm mt-1">
                 a passionate web developer creating reponsive and modren web
                 experiences. I specialize in React, Tailwind, CSS, and building
                 user-friendly interfaces.
               </p>
-              <botton className="lg:h-[9dvh]  h-[6dvh] lg:w-[9svw] text-gray-200 w-[23svw] font-bold rounded-md border-none bg-[rgb(172,129,239)] border text-white text-base lg:mt-10 mt-4 flex items-center justify-center lg:text-xl text-sm">
+              <botton className="p-4 font-bold rounded-md border-none bg-purple-800 border text-white text-base lg:mt-10 mt-4 flex items-center justify-center lg:text-xl text-sm">
                 SAY HELLO!
               </botton>
             </div>
-            <div className="h-1/4 w-full">
-              <div className="  md:text-xl text-sm lg:h-[10dvh] h-[10dvh] w-full text-white  grid grid-cols-3 gap-1 md:px-15  px-8 lg:mt-1 mt-5">
+            {/* <div className="h-1/4 w-full"> */}
+            {/* <div1 className=" hidden md:block md:text-xl text-sm lg:h-[10dvh] h-[10dvh] w-full text-white  grid grid-cols-3 gap-1 md:px-15  px-8 lg:mt-1 mt-5">
                 <dv className=" flex justify-center items-center  bg-[rgb(172,129,239)]  border ">
                   experience
                 </dv>
@@ -31,25 +35,40 @@ export default function Myinfo() {
                 <div className=" flex justify-center items-center border   bg-[rgb(172,129,239)]  ">
                   happy clients
                 </div>
-              </div>
-            </div>
+              </div1> */}
+            {/* </div> */}
           </div>
-          <div className=" md:h-[100dvh] md:w-1/2 w-full h-1/3 flex items-center md:justify-start justify-center">
-            <div className=" ml-10 md:h-[60dvh] h-[35dvh] w-[35dvh]  md:w-[60dvh] flex items-center justify-center border  border-none rounded-full  bg-gradient-to-tr from-[rgb(61,53,68)] to[rgba(198,252,216,1)]">
-              <div className="md:h-[55dvh] md:w-[55dvh]  h-[33dvh] w-[33dvh] bg-gradient-to-b from-[rgb(214,178,252)] to-[rgba(218,210,234,1)] border border-none rounded-full flex items-center justify-center">
-                <div className="md:h-[50dvh] md:w-[50dvh] h-[30dvh] w-[30dvh] flex items-center justify-center border rounded-full border-dashed  border-2 border-purple-400">
-                  <div className="bg-[rgb(172,129,239)] md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full">
-                    <img
-                      className="md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full  "
-                      src="/assets/images2.png"
-                    ></img>
-                  </div>
+          {/* <div className=" md:h-[100dvh] md:w-1/2 w-full h-1/3 flex items-center md:justify-start justify-center">
+            <div className=" ml-10 md:h-[60dvh] h-[35dvh] w-[35dvh]  md:w-[60dvh] flex items-center justify-center border  border-none rounded-full bg-purple-800"> */}
+          <div className="md:h-dvh md:w-1/2 w-full h-1/3 flex md:mt-25 mt-5  md:justify-start justify-center">
+            <div1 className="md:h-[55dvh] md:w-[55dvh]  h-[33dvh] w-[33dvh]  border border-none rounded-full flex items-center justify-center">
+              <div className="md:h-[50dvh] md:w-[50dvh] h-[30dvh] w-[30dvh] flex items-center justify-center  rounded-full border-dashed  border-2 border-purple-400">
+                <div className="bg-purple-800 md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full">
+                  <img
+                    className="md:h-[40dvh] md:w-[40dvh] h-[25dvh] w-[25dvh] brder rounded-full  "
+                    src="/assets/images2.png"
+                  ></img>
                 </div>
               </div>
-            </div>
+            </div1>
           </div>
         </div>
-        <div className="w-full h-[40dvh] bg-gray-800 ">h</div>
+        {/* </div>
+        </div> */}
+        <div className="w-full h-[40dvh] bg-gray-800 grid lg:grid-cols-3  md:grid-cols-2 text-white">
+          <div className="h-1/3 w-full flex flex-col">
+            <h1 className="text-2xl font-bold">Contact me</h1>
+            <h1>malikzafar0982@gmail.com</h1>
+            <h1>03446680619</h1>
+          </div>
+          <div className="h-1/3 w-full ">
+            <h1>Visit Me</h1>
+            <h1>Chak No 118/9-l, District Sahiwal</h1>
+          </div>
+          <div className="h-1/3 w-full">
+            <h1>Connect With Me</h1>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,43 +3,43 @@ import React from 'react'
 export default function skills() {
     return (
       <>
-        <div className="w-full h-screen  bg-gradient-to-tl from-white-500 via-[rgb(192,149,239)] to-[rgb(192,149,239)] flex items-center justify-center">
-          <div className="w-3/4 h-5/6 bg-purple-300 flex flex-col items-center border border-none rounded-2xl  shadow-2xl shadow-purple-400 ">
-            <div className="w-full h-[25dvh] bg-[rgb(122,96,160,1)] border border-none rounded-2xl flex flex-col items-center  text-gray-200 ">
-              <h1 className=" md:text-3xl md:font-bold  text-2xl font-semibold md:mt-3">
+        <div className="w-full h-screen bg-black flex items-center justify-center overflow-y-scroll ">
+          <div className="md:w-3/4 w-10/11 h-20/21  flex flex-col items-center border border-gray-400 rounded-2xl   shadow-[0_0_3px_rgba(255,255,255,0.3)]  ">
+            <div className="w-full h-[25dvh]  rounded-t-2xl flex flex-col px-2 items-center border border-gray-400 text-gray-200 ">
+              <h1 className=" md:text-3xl md:font-bold text-purple-800 text-2xl font-semibold md:mt-3">
                 What I do
               </h1>
-              <p className="md:text-base md:font-base  text-[12px] font-base">
+              <p className="md:text-sm text-[13px] md:font-base   font-base">
                 I am a Full Stack WebDeveloper skilled in HTML, CSS, JAVASCRIPT,
                 and modren frameworks like REACT. I build scalable backened
                 solutions using NODE.JS, EXPRESS.JS , MONGODB and folloe best
                 practises with GIT & GITHUB.
               </p>
             </div>
-            <div className=" md:w-3/4 w-full h-5/6  md:text-lg text-[11px]  text-purple-500 font-semibold  shadow-2xl shadow-purple-400">
-              <div className="w-full h-1/2 grid md:grid-cols-4 grid-cols-2 md:gap-12 gap-2  md:px-20 md:py-6 py-2 px-7 ">
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+            <div className=" md:w-3/4 w-full h-4/5  md:text-lg text-[11px] text-gray-200  font-semibold  shadow-2xl shadow-gray-900">
+              <div className="w-full h-1/2 grid md:grid-cols-4 grid-cols-2  gap-3 lg:gap-4 lg:px-18 md:py-4 py-2 px-7 ">
+                <div className=" transition duration-300 hover:scale-110   border rounded-2xl md:rounded-4xl border-gray-400  bg-black  flex items-center justify-center flex-col">
                   <img
                     className="md:h-[10dvh] h-[8dvh] object-contain "
                     src="/assets/css.png"
                   ></img>
                   CSS
                 </div>
-                <div className=" transition duration-300  hover:shadow-xl  hover:scale-110 border   rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300   hover:scale-110 border   rounded-2xl md:rounded-4xl border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className=" h-[9dvh] md:h-[10dvh]"
                     src="/assets/html.png"
                   ></img>
                   HTML
                 </div>
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl   border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh]"
                     src="/assets/java.png"
                   ></img>
                   JAVASCRIPT
                 </div>
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl  border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh]"
                     src="/assets/github.png"
@@ -47,29 +47,29 @@ export default function skills() {
                   GITHUB
                 </div>
               </div>
-              <div className="w-full h-1/2 grid md:grid-cols-4 grid-cols-2 gap-3 md:gap-12  md:px-20 md:py-6 py-2 px-7 ">
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+              <div className="w-full h-1/2 grid md:grid-cols-4 grid-cols-2 gap-3 lg:gap-4 lg:px-18 md:py-4 py-2 px-7 ">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl  border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh] "
                     src="/assets/mongodb.png"
                   ></img>
                   MONGO DB
                 </div>
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border rounded-2xl md:rounded-4xl  border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh]"
                     src="/assets/expressjs.png"
                   ></img>
                   EXPRESS.JS
                 </div>
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border  rounded-2xl md:rounded-4xl  border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh]"
                     src="/assets/react.png"
                   ></img>
                   REACT.JS
                 </div>
-                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border rounded-2xl md:rounded-4xl border-none shadow-2xl shadow-purple-400 bg-purple-300 flex items-center justify-center flex-col">
+                <div className=" transition duration-300 hover:scale-110 hover:shadow-xl border rounded-2xl md:rounded-4xl   border-gray-400  bg-black flex items-center justify-center flex-col">
                   <img
                     className="h-[8dvh] md:h-[10dvh]"
                     src="/assets/next.png"
@@ -78,8 +78,8 @@ export default function skills() {
                 </div>
               </div>
             </div>
-            <div className="w-full h-[1dvh] md:h-[15dvh] bg-[rgb(122,96,160,1)] border border-none rounded-2xl">
-              <h1 className=" md:text-3xl md:font-bold  text-gray-200 text-2xl font-semibold mt-3 flex justify-center">
+            <div className="w-full h-[10dvh] md:h-[15dvh]  border border-gray-400 rounded-b-2xl ">
+              <h1 className=" md:text-3xl md:font-bold  text-purple-800 text-2xl font-semibold mt-3 flex justify-center">
                 SKILLS
               </h1>
             </div>
